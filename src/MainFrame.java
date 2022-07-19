517649c5b94e59e5de00c2de21f5c80ea2495bcc
Innis <1575270674@qq.com>
2022-07-19 19:55:33 +0800

    build mianFrame

---
+++ src/MainFrame.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame(String title){
+        super(title);
+    }
+}
---
+++ src/Programs.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Programs {
+    public static void main(String[] args)
+    {
+        MainFrame frame = new MainFrame("APU Cafeteria Ordering System");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        frame.setSize(1200, 800);
+        frame.setVisible(true);
+    }
+
+}
